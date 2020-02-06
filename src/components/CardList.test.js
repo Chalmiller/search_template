d20f3b99366eaba8d0f5b7ea38e1992a35043a5a
Chalmiller <christopher@transcriptic.com>
2020-02-06 10:45:40 -0800

    Add Testingwith Jest

---
+++ src/components/CardList.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+import CardList from './CardList';
+import React from 'react';
+
+it('expect to render Card component', () => {
+    const mockRobots = [
+        {
+            id: 1,
+            name: 'John Snow',
+            username: 'JohnJohn',
+            email: 'john@gmail.com'
+        }
+    ]
+    expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+})
