cb7e2f16ceb39594a66782206b4d05151ba5557f
Chalmiller <christopher@transcriptic.com>
2020-02-07 10:57:54 -0800

    Add test coverage for more components

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
 
 const Card = (props) => {
     const { name, email, id } = props
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}props.id?200x200`}></img>
+            <img alt='robots' src={`https://robohash.org/${id}props.id?size=200x200`}></img>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
-import ErrorBoundry from '../components/ErrorBoundry';
-import Header from '../components/Header';
+import MainPage from '../components/MainPage';
 import './App.css';
 
 import { setSearchField, requestRobots } from '../action';
@@ -15,7 +11,6 @@ const mapStateToProps = (state) => {
         searchField: state.searchRobots.searchField,
         robots: state.requestRobots.robots,
         isPending: state.requestRobots.isPending,
-        error: state.requestRobots.error
     }
 }
 
@@ -27,29 +22,9 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 class App extends Component {
-    componentDidMount() {
-        this.props.onRequestRobots();
-    }
-
     render () {
-        const { searchField, onSearchChange, robots, isPending } = this.props
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase())
-        });
-        return (isPending) ? 
-        <h1>Loading...</h1> : 
-        (
-            <div className='tc'>
-                <Header />
-                <SearchBox searchChange={onSearchChange} />
-                <Scroll>
-                    <ErrorBoundry>
-                        <CardList robots={ filteredRobots } />        
-                    </ErrorBoundry>
-                </Scroll>
-            </div>   
-        );        
-    }
+        return <MainPage { ...this.props }/>
+    };        
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
